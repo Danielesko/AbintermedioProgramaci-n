569d78470bad669d4db6c911c96318f97d15b0a1
Danielesko <unan1515@gmail.com>
2024-01-20 13:35:56 +0100

    Creacion funciones php

---
+++ funciones.php
@@ -0,0 +1,34 @@
+<?php
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+if($nombre == "" || $correo == "" || $mensaje == ""){
+    ?>
+    <script>
+        alert("No puedes enviar datos vacíos");
+    </script>
+    <?php
+    header("location:pages/contacto.html");
+} else {
+    $conn = new mysqli("localhost", "usuario", "contraseña", "basededatos");
+
+    if ($conn->connect_error) {
+        die("La conexión a la base de datos falló: " . $conn->connect_error);
+    }
+
+    $stmt = $conn->prepare("INSERT INTO contacto (correo, nombre, mensaje) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $correo, $nombre, $mensaje);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    ?>
+    <script>
+        alert("Mensaje enviado correctamente");
+    </script>
+    <?php
+    header("location:pages/contacto.html");
+}
+
+
+?>
